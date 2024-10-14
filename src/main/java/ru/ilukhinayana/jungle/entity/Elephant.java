faa3f04e97555ca8fcae7d087baf84f67ab6143d
yana-ilukhina <ilukhina.yana@gmail.com>
2024-10-14 15:24:39 +0300

    ыÑinit setters and getters in class entity

--- src/main/java/ru/ilukhinayana/jungle/entity/Elephant.java
+++ src/main/java/ru/ilukhinayana/jungle/entity/Elephant.java
@@ -1,9 +1,27 @@
 package ru.ilukhinayana.jungle.entity;
 
 public class Elephant {
-    private int health = 120;
-    private int energy = 100;
-    private int fangs = 2;
+    private int health = 120; // здоровье животного
+    private int energy = 100; // энергия животного
+    private final int FANGS = 2; // коэффициент клыков у животного
 
+    public void setHealth(int health) {
+        this.health = health;
+    }
 
+    public int getHealth() {
+        return health; //(this.?)
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public int getFANGS() {
+        return FANGS;
+    }
 }
