37cc14682a7c859cb2d7930474152b9fc4e3f81e
yana-ilukhina <ilukhina.yana@gmail.com>
2024-10-15 17:39:13 +0300

    add Ñd neutral and positive events

--- src/main/java/ru/ilukhinayana/jungle/util/Event.java
+++ src/main/java/ru/ilukhinayana/jungle/util/Event.java
@@ -6,7 +6,7 @@ public class Event {
     //private static final int FANGS = 2;
 
     // 1. слон поспал +20 энергии +5 здоровья
-    // 2. слон побегал, ничего не нашел -7 энергии
+    // 2. слон походил, ничего не нашел -7 энергии
     // 3. слон съел листьев + коэфф * 5 энергии и + 5 здоровья
     // 4. слон съел фруктов + коэфф * 4 энергии и + 5 здоровья
     // 5. слон попил воды, + коэфф * 4 энергии и + 2 здоровья
@@ -23,41 +23,67 @@ public class Event {
         //sleepEvent();
     }
 
-    private void sleepEvent(Elephant eleph) {
+    private void sleepEvent(Elephant eleph) { // 1. слон поспал: +20 энергии +5 здоровья
         int energy = eleph.getEnergy();
         int health = eleph.getHealth();
         energy += 20;
         health += 5;
-        if (energy > 100) {
-            energy = 100;
-            }
+        checkMinMaxEnergy(eleph);
+        checkMinMaxHealth(eleph);
         eleph.setEnergy(energy);
-        System.out.println("Слон поспал! + 5 здоровья, + 20 энергии. Текущие показатели здоровья и энергии: "
-                + eleph.getHealth() + " " + eleph.getEnergy());
+        eleph.setHealth(health);
+        System.out.println("Слон поспал! +5 здоровья, +20 энергии. Текущие показатели: "
+                + eleph.getHealth() + " здоровья, " + eleph.getEnergy() + "энергии");
     }
 
-    private void move(Elephant eleph) {
+    private void move(Elephant eleph) { // 2. слон походил, ничего не нашел: -7 энергии
         int energy = eleph.getEnergy();
         energy -= 7;
-        if (energy < 0) {
-            energy = 0;
-        }
+        checkMinMaxEnergy(eleph);
         eleph.setEnergy(energy);
-        System.out.println("Слон походил просто так! -7 энергии. Текущие показатели здоровья и энергии: "
-                + eleph.getHealth() + " " + eleph.getEnergy());
+        System.out.println("Слон походил просто так, немного устал! -7 энергии. Текущие показатели: "
+                + eleph.getHealth() + " здоровья, " + eleph.getEnergy() + "энергии");
     }
 
-    private void eatLeaves(Elephant eleph) {
+    private void eatLeaves(Elephant eleph) { // 3. слон поел листьев: 5 энергии * коэфф клык и + 5 здоровья
         int energy = eleph.getEnergy();
-        energy = energy + (4 * eleph.getFANGS());
+        int health = eleph.getHealth();
+        energy = energy + (5 * eleph.getFANGS());
+        health += 5;
         checkMinMaxEnergy(eleph);
-        if (energy < 100) {
-            energy = 100;
-        }
+        checkMinMaxHealth(eleph);
         eleph.setEnergy(energy);
-        System.out.println("Слон побегал впустую! Текущие показатели здоровья и энергии: "
-                + eleph.getHealth() + " " + eleph.getEnergy());
+        eleph.setHealth(health);
+        System.out.println("Слон поел листьев! :) +5 здоровья, +10 энергии. Текущие показатели: "
+                + eleph.getHealth() + " здоровья, " + eleph.getEnergy() + "энергии");
     }
+
+    private void eatFruits(Elephant eleph) { // 4. слон поел фруктов: 4 энергии * коэфф клык и + 5 здоровья
+        int energy = eleph.getEnergy();
+        int health = eleph.getHealth();
+        energy = energy + (4 * eleph.getFANGS());
+        health += 5;
+        checkMinMaxEnergy(eleph);
+        checkMinMaxHealth(eleph);
+        eleph.setEnergy(energy);
+        eleph.setHealth(health);
+        System.out.println("Слон поел фруктов! :) +5 здоровья, +8 энергии. " +
+                "Текущие показатели: " + eleph.getHealth() + " здоровья, " + eleph.getEnergy() + "энергии");
+    }
+
+    private void drinkWater(Elephant eleph) { // 5. слон попил воды: 2 энергии * коэфф клык и + 2 здоровья
+        int energy = eleph.getEnergy();
+        int health = eleph.getHealth();
+        energy = energy + (2 * eleph.getFANGS());
+        health += 2;
+        checkMinMaxEnergy(eleph);
+        checkMinMaxHealth(eleph);
+        eleph.setEnergy(energy);
+        eleph.setHealth(health);
+        System.out.println("Слон попил воды! :) +2 здоровья, +4 энергии. " +
+                "Текущие показатели: " + eleph.getHealth() + " здоровья, " + eleph.getEnergy() + "энергии");
+    }
+
 
     private void checkMinMaxHealth(Elephant eleph) {
         int health = eleph.getHealth();
@@ -79,13 +105,13 @@ public class Event {
         if (energy > 100) {
             energy = 100;
             eleph.setEnergy(energy);
-            System.out.println("Слон и так бодрый! Текущие показатели здоровья и энергии: "
+            System.out.println("Слон и так бодрый! Текущие показатели: "
                     + eleph.getHealth() + " здоровья,  " + eleph.getEnergy() + " энергии");
         }
         if (energy <= 0) {
             energy = 0;
             eleph.setEnergy(energy);
-            System.out.println("Слон очень устал! Текущие показатели здоровья и энергии: "
+            System.out.println("Слон очень устал! Текущие показатели: "
                     + eleph.getHealth() + " здоровья,  " + eleph.getEnergy() + " энергии");
         }
     }
